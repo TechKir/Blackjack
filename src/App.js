9e360de1290505e4b58ec490c25b67df8f5a1480
Wojciech Kiryk <wojciech.kiryk@gmail.com>
2021-02-02 17:15:08 +0100

    add attribution, flavicon,new feature: ask user to change portrait orientation to landscape - mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React,{useState, createContext} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React,{useState, useEffect, createContext} from 'react';
 import BjHeader from './components/BjHeader';
 import GameArea from './components/GameArea';
+import classnames from 'classnames';
 import ScoresNRoundsBox from './components/ScoresNRoundsBox';
 export const AuthContext = createContext(null); 
 
 function App() {
+
   const ls = window.localStorage;
 
   //STARTING CASH:
@@ -21,16 +24,35 @@ function App() {
   const [isScoreReset,setIsScoreReset]=useState(false);
   const [rerender,setRerender]=useState(false);
 
+  //Mobile - ask user to rotate phone to horizontal position:
+  const orientation = window.screen.orientation;
+  const [isVisible,setIsVisible]=useState(true);
+
+  useEffect(()=>{
+    if(orientation.type==="portrait-primary"){
+      setIsVisible(false);
+    }
+  },[])
+
+  window.addEventListener('orientationchange', ()=>{
+    if(orientation.type==="portrait-primary"){
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  });
+
   return (
     <AuthContext.Provider 
     value={{leftCash, setLeftCash, bet, setBet, isPlay, setIsPlay, round, setRound, doubleDown, setDoubleDown, isWin, setIsWin, isScoreReset, setIsScoreReset, rerender, setRerender}}>
-      <div className="App">
+      <div className={classnames('App', { inVisible: !isVisible })}>
         <BjHeader/>
         <div className='gameContainer'>
           <GameArea/>
           <ScoresNRoundsBox/>
         </div>
       </div>
+      <div className={classnames('attribution', { inVisible: isVisible })}>Phone and cursor icon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, Card favicon made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </AuthContext.Provider>      
   );
 }
